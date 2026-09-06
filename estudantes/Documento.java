c3e1beb92b83a9f0447ce9ba27c3fea486b4bf2f
Felipe Burmann <lipeburmann720@gmail.com>
2026-09-06 09:48:18 -0300

    arrumada as pastas de codigos

---
+++ estudantes/Documento.java
@@ -0,0 +1,40 @@
+package estudantes.entidades;
+
+import professor.entidades.CodigoCurso;
+
+/**
+ * Classe que representa um documento genérico.
+ * <br><br>
+ * <strong>Seu trabalho começa aqui...</strong>
+ * 
+ * @author coloque os nomes dos autores aqui
+ */
+public abstract class Documento {
+    private String criador;
+    private CodigoCurso codigoCurso;
+    private int paginas;
+
+    public Documento(String criador, CodigoCurso codigoCurso, int paginas) {
+        this.criador = criador;
+        this.codigoCurso = codigoCurso;
+        this.paginas = paginas;
+    }
+
+    public String getCriador() {
+        return criador;
+    }
+
+    public CodigoCurso getCodigoCurso() {
+        return codigoCurso;
+    }
+
+    public int getPaginas() {
+        return paginas;
+    }
+
+    @Override
+    public abstract boolean equals(Object obj);
+
+    @Override
+    public abstract int hashCode();
+}
